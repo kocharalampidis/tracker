6fb7cba50a2980d2fecbbd92db36c4086e8a269a
kocharalampidis <kocharalampidis@gmail.com>
2023-01-28 17:15:14 +0200

    added github and linkedin links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,10 +8,20 @@ const Footer = () => (
       <p>Copyright © 2023 - All right reserved</p>
     </div>
     <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-      <Link className="text-white" href={""}>
+      <Link
+        className="text-white"
+        href={"https://github.com/kocharalampidis"}
+        target="_blank"
+      >
         <BsGithub size="25px" />
       </Link>
-      <Link className="text-white" href={""}>
+      <Link
+        className="text-white"
+        href={
+          "https://www.linkedin.com/in/konstantinos-charalampidis-45b6ba1b3/"
+        }
+        target="_blank"
+      >
         <BsLinkedin size="25px" />
       </Link>
     </div>
